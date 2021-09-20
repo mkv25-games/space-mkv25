90a516d883d986c11a2934526b9734f722e9d515
John Beech <johnbeech.github@mkv25.net>
2021-09-21 00:12:58 +0100

    Store increments to userPreferences file on local machine.

--- mainui/src/mainui.js
+++ mainui/src/mainui.js
@@ -4,3 +4,26 @@ import router from './router'
 import store from './store/root'
 
 createApp(App).use(store).use(router).mount('#mainui-app')
+
+function clone (data) {
+  return JSON.parse(JSON.stringify(data))
+}
+
+const noop = () => {}
+const history = []
+const subscriptions = {
+  increment: (state) => {
+    console.log('Incrementing data based on user preference', state)
+    window.electron.sendData('userPreferences', state)
+  }
+}
+store.subscribeAction({
+  before: (action, state) => {
+    history.push({ timestamp: Date.now(), type: action.type, state })
+  },
+  after: (action, state) => {
+    console.log('Action', action, state)
+    const fn = (subscriptions[action.type] || noop)
+    fn(clone(state))
+  }
+})
--- mainui/src/views/Settings.vue
+++ mainui/src/views/Settings.vue
@@ -2,8 +2,11 @@
   <div class="settings">
     <h1>This is the settings page</h1>
     <p>Store Count: {{ $store.state.count }}</p>
-    <button v-on:click="$store.dispatch('increment')">Increment</button>
-    <button v-on:click="$store.dispatch('hideDeveloperTools')" v-if="$store.state.developerTools.visible">Hide Developer Tools</button>
-    <button v-on:click="$store.dispatch('showDeveloperTools')" v-else>Show Developer Tools</button>
+    <h2>Interactions</h2>
+    <p><button v-on:click="$store.dispatch('increment')">Increment</button></p>
+    <p>
+      <button v-on:click="$store.dispatch('hideDeveloperTools')" v-if="$store.state.developerTools.visible">Hide Developer Tools</button>
+      <button v-on:click="$store.dispatch('showDeveloperTools')" v-else>Show Developer Tools</button>
+    </p>
   </div>
 </template>
