7a8d7ea59cc4c94fc2af25a6adc9369c540d4ff2
John Beech <jbeech@marqeta.com>
2021-10-18 23:20:23 +0100

    Extract some data from region SVG

--- mainui/src/views/omniscience/Regions.vue
+++ mainui/src/views/omniscience/Regions.vue
@@ -3,12 +3,15 @@
     <column-layout class="fixed-width-right overflow-hidden">
       <template v-slot:left>
         <slot-viewer>
-          <RegionsSVG style="width: 500px; height: 500px;" />
+          <div ref="region-svg">
+            <RegionsSVG style="width: 500px; height: 500px;" />
+          </div>
         </slot-viewer>
       </template>
       <template v-slot:right>
         <h2>Regions</h2>
         <p>Analyze regions</p>
+        <div>{{ analysis }}</div>
       </template>
     </column-layout>
   </OmniscienceView>
@@ -24,13 +27,37 @@ import SlotViewer from '@/components/ui/SlotViewer.vue'
 import RegionsSVG from '@/models/visual/regions.drawio.svg'
 
 export default {
+  data() {
+    return {
+      visualModel: false
+    }
+  },
   components: {
     OmniscienceView, ColumnLayout, Icon, RegionsSVG, SlotViewer
   },
   computed: {
-    galaxy () {
+    galaxy() {
       return this.contact.galaxy || newGalaxy()
+    },
+    analysis() {
+      const model = this.visualModel
+      if (model) {
+        const paths = [...model.getElementsByTagName('path')].map(p => p.getAttribute('d'))
+        const labels = [...model.getElementsByTagName('text')].map(l => l.textContent)
+        const data = paths.map((path, i) => {
+          const label = labels[i]
+          return { label, path }
+        })
+        return data
+      }
+      return []
     }
+  },
+  mounted() {
+    const self = this
+    this.$nextTick(() => {
+      self.visualModel = self.$refs['region-svg']
+    })
   }
 }
 </script>
