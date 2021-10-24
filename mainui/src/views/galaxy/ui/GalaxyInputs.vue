703916cdc6512028a4619e1dd26b0b5c71c0124f
John Beech <jbeech@marqeta.com>
2021-10-24 14:46:11 +0100

    Rework galaxy seeding to produce more variation

--- mainui/src/views/galaxy/ui/GalaxyInputs.vue
+++ mainui/src/views/galaxy/ui/GalaxyInputs.vue
@@ -2,10 +2,18 @@
   <div class="galaxy-input">
     <property label="Galaxy width">
       <input v-model="createGalaxyWidth" type="number" min="5" max="50" v-on:change="checkForm">
-      <icon icon="arrows-alt-h" />
+      <icon icon="long-arrow-alt-right" />
     </property>
     <property label="Galaxy height">
       <input v-model="createGalaxyHeight" type="number" min="5" max="50" v-on:change="checkForm">
+     <icon icon="long-arrow-alt-up" />
+    </property>
+    <property label="X Offset">
+      <input v-model="createGalaxyXOffset" type="number" min="-65536" max="65536" v-on:change="checkForm">
+      <icon icon="arrows-alt-h" />
+    </property>
+    <property label="Y Offset">
+      <input v-model="createGalaxyYOffset" type="number" min="-65536" max="65536" v-on:change="checkForm">
      <icon icon="arrows-alt-v" />
     </property>
     <property label="Galaxy seed">
@@ -34,6 +42,8 @@ export default {
       createGalaxySeed: 0,
       createGalaxyWidth: 10,
       createGalaxyHeight: 10,
+      createGalaxyXOffset: 0,
+      createGalaxyYOffset: 0,
       quadrantFunction: 'perlin'
     }
   },
@@ -47,6 +57,10 @@ export default {
         size: {
           w: Math.min(this.createGalaxyWidth, 50),
           h: Math.min(this.createGalaxyHeight, 50)
+        },
+        offset: {
+          x: Math.max(-65536, Math.min(65536, this.createGalaxyXOffset)),
+          y: Math.max(-65536, Math.min(65536, this.createGalaxyYOffset))
         },
         seed: Math.min(this.createGalaxySeed, 65536),
         qFn: this.quadrantFunction || 'perlin'
