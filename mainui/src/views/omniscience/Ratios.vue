bf5c3f50198d1e996d6019383947730d26ccc370
John Beech <jbeech@marqeta.com>
2022-01-21 16:28:13 +0000

    Cobble together ratio converter; unable to parse numbers correctly or perform multi-step lookup

--- mainui/src/views/omniscience/Ratios.vue
+++ mainui/src/views/omniscience/Ratios.vue
@@ -5,6 +5,7 @@
       <property label="Quantity"><input type="text" placeholder="Conversion amount?" v-model="quantity" /></property>
       <property label="Target unit"><input type="text" placeholder="Conversion unit?" v-model="targetUnit" /></property>
       <property label="Result">{{ result }}</property>
+      <p>{{ debug }}</p>
     </div>
     <div class="dataset">
       <h3>Ratios</h3>
@@ -23,14 +24,34 @@
 
 <script>
 
-function findRatio(input, ratios) {
-  const [value,suffix] = input.split('')
-  const ratio = ratios.filter(ratio => ratio.from === suffix)[0] || {}
-  return ratio.name
+function parseNumberString(numberString) {
+  return Number.parseFloat(numberString)
 }
 
-function findQuantity(input, units) {
-  return 5
+function parseRatioToNumber(ratioString) {
+  const [left,right] = ratioString.split(':').map(parseNumberString)
+  return left / right
+}
+
+function findRatio(targetUnit, ratiosFrom, ratiosTo) {
+  let target = ratiosTo[targetUnit]
+  if (target) {
+    target.ratioAsNumber = parseRatioToNumber(target.ratio)
+  }
+  return target
+}
+
+function clone(obj) {
+  return JSON.parse(JSON.stringify(obj))
+}
+
+function parseQuantityUnit(input, unitIndex) {
+  const [quantity, suffix] = input.split(' ')
+  const unit = clone(unitIndex[suffix] || { suffix })
+  return {
+    quantity,
+    unit
+  }
 }
 
 function calculateConversion(inputRatio, targetRatio) {
@@ -41,7 +62,8 @@ export default {
   data() {
     return {
       quantity: '',
-      targetUnit: ''
+      targetUnit: '',
+      debug: ''
     }
   },
   computed: {
@@ -60,17 +82,21 @@ export default {
     units() {
       return this.gamedata.Unit || []
     },
+    unitIndex() {
+      return this.$store.getGamedataIndex('Unit', 'suffix')
+    },
     result() {
-      const { quantity, targetUnit, ratios, units } = this
-      const quantityUnit = findQuantity(quantity, units)
-      const targetRatio = findRatio(targetUnit, ratios)
+      const { quantity, targetUnit, ratiosFrom, ratiosTo, unitIndex } = this
+      const quantityUnit = parseQuantityUnit(quantity, unitIndex)
+      const targetRatio = findRatio(targetUnit, ratiosFrom, ratiosTo)
       if (!quantityUnit) {
         return 'No quantity unit found'
       }
       if (!targetRatio) {
         return 'No target ratio found'
       }
-      return [quantityUnit, targetRatio].join(' needs converting to ')
+      this.debug = [quantityUnit, targetRatio].map(JSON.stringify).join(' needs converting to ')
+      return [targetRatio.ratioAsNumber * quantityUnit.quantity, targetRatio.to].join(' ')
     }
   }
 }
